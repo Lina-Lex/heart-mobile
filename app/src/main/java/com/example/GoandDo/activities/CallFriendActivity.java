96eb2f84209799a22b19ec740a62e191ea4bda1a
Gideon Ukoko Jr <gideon.ukoko98@gmail.com>
2021-08-20 15:49:01 +0100

    Initial activity_call_friend.xml Setup

---
+++ app/src/main/java/com/example/GoandDo/activities/CallFriendActivity.java
@@ -0,0 +1,14 @@
+package com.example.GoandDo.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CallFriendActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call_friend);
+    }
+}
