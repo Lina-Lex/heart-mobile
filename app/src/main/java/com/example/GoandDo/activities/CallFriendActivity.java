0539eb778848966d685e48e560351ae8d546a8cd
Gideon Ukoko Jr <gideon.ukoko98@gmail.com>
2021-08-23 00:24:56 +0100

    Dummy Data Implementation For Contact List

--- app/src/main/java/com/example/GoandDo/activities/CallFriendActivity.java
+++ app/src/main/java/com/example/GoandDo/activities/CallFriendActivity.java
@@ -1,16 +1,62 @@
 package com.example.GoandDo.activities;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.GoandDo.R;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class CallFriendActivity extends AppCompatActivity {
 
+    ListView listView;
+    String sName[] = {"ANN YOUNG", "CHRISTOPHER SULLIVAN", "CARTER WORKMAN", "JUDY BISHOP", "LILY MILLER", "PATRICK HILL", "RUTH MYERS"};
+    int images[] = {R.drawable.profile_image, R.drawable.profile_image, R.drawable.profile_image, R.drawable.profile_image, R.drawable.profile_image, R.drawable.profile_image, R.drawable.profile_image};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call_friend);
+
+        listView = findViewById(R.id.listView);
+
+        MyAdapter adapter = new MyAdapter(this, sName, images);
+        listView.setAdapter(adapter);
+    }
+
+    class MyAdapter extends ArrayAdapter<String> {
+        Context context;
+        String rName[];
+        int rImage[];
+
+        MyAdapter (Context c, String name[], int img[]){
+            super(c, R.layout.contact_list_dummy_items, R.id.dummyProfileText, name);
+            this.context = c;
+            this.rName = name;
+            this.rImage = img;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater layoutInflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View dummy_layout = layoutInflater.inflate(R.layout.contact_list_dummy_items, parent, false);
+            CircleImageView profileImg = dummy_layout.findViewById(R.id.dummyProfileImg);
+            TextView profileName = dummy_layout.findViewById(R.id.dummyProfileText);
+
+            profileImg.setImageResource(rImage[position]);
+            profileName.setText(rName[position]);
+
+            return dummy_layout;
+        }
     }
 }
